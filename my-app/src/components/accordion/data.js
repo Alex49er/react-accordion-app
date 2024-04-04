716faa3b13056f924d0d4e0d79b15005663aa190
Alex49er <151443937+Alex49er@users.noreply.github.com>
2024-04-04 18:42:55 +0100

    created the npm for the accordion app, added data, began building the app.js

---
+++ my-app/src/components/accordion/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        id : '1',
+        question: "What are accordion component?",
+        answer: "Accordion components are used for interface elements",
+    },
+    {
+        id : '2',
+        question: "What are they used for?",
+        answer: "They are commonly empolyed in various contexts",
+    },
+    {
+        id : '3',
+        question: "What is an accordion as a musical instrument?",
+        answer: "The accordion is an instrument with a keyboard and bellows",
+    },
+    {
+        id : '4',
+        question: "Can I create an accordion with different components?",
+        amswer: "Yes it is possible to create an accordion with different components"
+    },
+];
